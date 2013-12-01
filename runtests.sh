250fb7cbc00532b48142a19486a01ab5c6c655aa
Dario Marcelino <warchild@warchild.lan>
2013-11-30 23:32:10 -0500

    Added build command to runtests.sh via flag

--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+ 
+usage() { echo "Usage: $0 [-b]" >&2; exit 1; }
+ 
+while getopts ":b" opt; do
+  case $opt in
+    b)
+      echo "Building titanium..." >&2
+      titanium build -p mobileweb
+      ;;
+    \?)
+      usage
+      ;;
+  esac
+done
+
 phantomjs --web-security=no --local-to-remote-url-access=yes tests.js
